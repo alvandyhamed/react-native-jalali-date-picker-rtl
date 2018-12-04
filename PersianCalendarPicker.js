62974d591e2f98e8a83f7e76f30d354e8f6cf4eb
hamed alvandi <alvandyhamed@gmail.com>
2018-12-04 15:22:40 +0330

    selectibale yeares

--- PersianCalendarPicker.js
+++ PersianCalendarPicker.js
@@ -11,8 +11,8 @@ const React = require('react');
 const PropTypes = require('prop-types');
 const moment = require('moment-jalaali');
 const {
-  Text,
-  View,
+    Text,
+    View,
 } = require('react-native');
 
 const styles = require('./style');
@@ -21,130 +21,149 @@ const WeekDaysLabels = require('./WeekDaysLabels');
 const Days = require('./Days');
 
 class PersianCalendarPicker extends React.Component {
-  static propTypes = {
-    maxDate: PropTypes.instanceOf(Date),
-    minDate: PropTypes.instanceOf(Date),
-    selectedDate: PropTypes.instanceOf(Date).isRequired,
-    onDateChange: PropTypes.func,
-    screenWidth: PropTypes.number,
-    weekdays: PropTypes.array,
-    months: PropTypes.array,
-    previousTitle: PropTypes.string,
-    nextTitle: PropTypes.string,
-    selectedDayColor: PropTypes.string,
-    selectedDayTextColor: PropTypes.string,
-    scaleFactor: PropTypes.number,
-    textStyle: Text.propTypes.style,
-    prevDays: PropTypes.string,
-    rtl: PropTypes.bool
+    static propTypes = {
+        maxDate: PropTypes.instanceOf(Date),
+        minDate: PropTypes.instanceOf(Date),
+        selectedDate: PropTypes.instanceOf(Date).isRequired,
+        onDateChange: PropTypes.func,
+        screenWidth: PropTypes.number,
+        weekdays: PropTypes.array,
+        months: PropTypes.array,
+        previousTitle: PropTypes.string,
+        nextTitle: PropTypes.string,
+        selectedDayColor: PropTypes.string,
+        selectedDayTextColor: PropTypes.string,
+        scaleFactor: PropTypes.number,
+        textStyle: Text.propTypes.style,
+        prevDays: PropTypes.string,
+        rtl: PropTypes.bool
 
-  };
-
-  constructor(props) {
-    super(props);
-    // if (this.props.scaleFactor !== undefined) {
-    //   styles = StyleSheet.create(makeStyles(this.props.scaleFactor));
-    // }
-    let date = moment(this.props.selectedDate);
-    this.state = {
-      date: date,
-      day: date.jDate(),
-      month: date.jMonth(),
-      year: date.jYear(),
-      selectedDay: []
     };
 
-    this.onDayChange = this.onDayChange.bind(this);
-    this.onMonthChange = this.onMonthChange.bind(this);
-    this.getNextYear = this.getNextYear.bind(this);
-    this.getPrevYear = this.getPrevYear.bind(this);
-    this.onDateChange = this.onDateChange.bind(this);
-  }
+    constructor(props) {
+        super(props);
+        // if (this.props.scaleFactor !== undefined) {
+        //   styles = StyleSheet.create(makeStyles(this.props.scaleFactor));
+        // }
+        let date = moment(this.props.selectedDate);
+        this.state = {
+            date: date,
+            day: date.jDate(),
+            month: date.jMonth(),
+            year: date.jYear(),
+            selectedDay: []
+        };
 
-  // Trigger date change if new props are provided.
-  // Typically, when selectedDate is changed programmatically.
-  //
-  componentWillReceiveProps(newProps) {
-    let date = moment(newProps.selectedDate);
-    this.setState({
-      date: date,
-      day: date.jDate(),
-      month: date.jMonth(),
-      year: date.jYear(),
-    });
-  }
 
-  onDayChange(day) {
+        this.onDayChange = this.onDayChange.bind(this);
+        this.onMonthChange = this.onMonthChange.bind(this);
+        this.getNextYear = this.getNextYear.bind(this);
+        this.getPrevYear = this.getPrevYear.bind(this);
+        this.onDateChange = this.onDateChange.bind(this);
+        this.setYear = this.setYear.bind(this)
+    }
 
-    this.setState({ day: day.day }, () => { this.onDateChange(); });
-  }
+    // Trigger date change if new props are provided.
+    // Typically, when selectedDate is changed programmatically.
+    //
+    componentWillReceiveProps(newProps) {
+        let date = moment(newProps.selectedDate);
+        this.setState({
+            date: date,
+            day: date.jDate(),
+            month: date.jMonth(),
+            year: date.jYear(),
+        });
+    }
 
-  onMonthChange(month) {
-    this.setState({ month: month }, () => { this.onDateChange(); });
-  }
+    onDayChange(day) {
 
-  getNextYear() {
-    this.setState({ year: this.state.year + 1 }, () => { this.onDateChange(); });
-  }
+        this.setState({day: day.day}, () => {
+            this.onDateChange();
+        });
+    }
 
-  getPrevYear() {
-    this.setState({ year: this.state.year - 1 }, () => { this.onDateChange(); });
-  }
+    onMonthChange(month) {
+        this.setState({month: month}, () => {
+            this.onDateChange();
+        });
+    }
 
-  onDateChange() {
-    let {
-      day,
-      month,
-      year,
-    } = this.state;
-    let date = moment(year + '/' + (month + 1) + '/' + day, 'jYYYY/jM/jD');
-    let date2 = new Date(date.year(), date.month(), date.date());
-    console.log(this.props);
-    console.log(this.state);
-    this.setState({ date: date });
-    this.props.onDateChange(date2);
-  }
+    getNextYear() {
+        this.setState({year: this.state.year + 1}, () => {
+            this.onDateChange();
+        });
+    }
 
-  render() {
-    return (
-      <View style={styles.calendar}>
-        <HeaderControls
-          maxDate={this.props.maxDate}
-          minDate={this.props.minDate}
-          year={this.state.year}
-          month={this.state.month}
-          onMonthChange={this.onMonthChange}
-          getNextYear={this.getNextYear}
-          getPrevYear={this.getPrevYear}
-          months={this.props.months}
-          previousTitle={this.props.previousTitle}
-          nextTitle={this.props.nextTitle}
-          textStyle={this.props.textStyle}
-          reverse={this.props.reverse}
-        />
-        <WeekDaysLabels
-          screenWidth={this.props.screenWidth}
-          weekdays={this.props.weekdays}
-          textStyle={this.props.textStyle}
-          reverse={this.props.reverse}
-        />
-        <Days
-          maxDate={this.props.maxDate}
-          minDate={this.props.minDate}
-          month={this.state.month}
-          year={this.state.year}
-          date={this.state.date}
-          onDayChange={this.onDayChange}
-          screenWidth={this.props.screenWidth}
-          selectedDayTextColor={this.props.selectedDayTextColor}
-          selectedDayColor={this.props.selectedDayColor}
-          textStyle={this.props.textStyle}
-          prevDays={this.props.prevDays}
-          reverse={this.props.reverse}
-        />
-      </View>
-    );
-  }
+    setYear(years) {
+
+        this.setState({year: years}, () => {
+            this.onDateChange();
+        });
+    }
+
+
+    getPrevYear() {
+        this.setState({year: this.state.year - 1}, () => {
+            this.onDateChange();
+        });
+    }
+
+
+    onDateChange() {
+        let {
+            day,
+            month,
+            year,
+        } = this.state;
+        let date = moment(year + '/' + (month + 1) + '/' + day, 'jYYYY/jM/jD');
+        let date2 = new Date(date.year(), date.month(), date.date());
+
+        this.setState({date: date});
+        this.props.onDateChange(date2);
+    }
+
+    render() {
+        return (
+            <View style={styles.calendar}>
+                <HeaderControls
+                    maxDate={this.props.maxDate}
+                    minDate={this.props.minDate}
+                    year={this.state.year}
+                    month={this.state.month}
+                    onMonthChange={this.onMonthChange}
+                    getNextYear={this.getNextYear}
+                    getPrevYear={this.getPrevYear}
+                    setYeare={this.setYear}
+                    months={this.props.months}
+                    previousTitle={this.props.previousTitle}
+                    nextTitle={this.props.nextTitle}
+                    textStyle={this.props.textStyle}
+                    reverse={this.props.reverse}
+                />
+                <WeekDaysLabels
+                    screenWidth={this.props.screenWidth}
+                    weekdays={this.props.weekdays}
+                    textStyle={this.props.textStyle}
+                    reverse={this.props.reverse}
+                />
+                <Days
+                    maxDate={this.props.maxDate}
+                    minDate={this.props.minDate}
+                    month={this.state.month}
+                    year={this.state.year}
+                    date={this.state.date}
+                    onDayChange={this.onDayChange}
+                    screenWidth={this.props.screenWidth}
+                    selectedDayTextColor={this.props.selectedDayTextColor}
+                    selectedDayColor={this.props.selectedDayColor}
+                    textStyle={this.props.textStyle}
+                    prevDays={this.props.prevDays}
+                    reverse={this.props.reverse}
+                />
+            </View>
+        );
+    }
 }
 
 module.exports = PersianCalendarPicker;
